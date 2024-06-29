b82fb1072944748253fac7bf4f8b591e49e22431
Dylan Ravel <dylanm.ravel@gmail.com>
2024-06-28 20:44:06 -0600

    Profile Project - Codecademy

---
+++ Projects/Personal/Codecademy/ObjectOfYourAffection/app.cpp
@@ -0,0 +1,12 @@
+#include "profile.hpp"
+
+int main() {
+    Profile sam = Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+
+    sam.add_hobby("listening to audiobooks and podcasts");
+    sam.add_hobby("playing rec sports like bowling and kickball");
+    sam.add_hobby("writing a speculative fiction novel");
+    sam.add_hobby("reading advice columns");
+
+    std::cout << sam.view_profile() << std::endl;
+}
