88a7695b2df3f9181ca20eb1e10d152c533fd513
Dylan Ravel <dylanm.ravel@gmail.com>
2024-12-03 17:40:34 -0800

    Created folder for PA6

---
+++ Projects/School/PA6/README.txt
@@ -0,0 +1,17 @@
+AUTHOR INFO
+Full name: Dylan M. Ravel
+Student ID: 2445987
+Chapman Email: ravel@chapman.edu
+Course number and section: CPSC-350-03
+Assignment or exercise number: PA6: Spanning the Gamut
+
+ERRORS 
+There are no errors from what I have observed so far.
+
+SOURCES
+Below are all the sources I used throughout my project. Next to each link is how it helped me specifically with the project.
+    - 
+
+RUNNING INSTRUCTIONS
+Compile the file as so: "g++ *.cpp -o program.out"
+Run the file as so: "./program.out INPUT_FILE.txt" ( !!! Make sure to include file extension !!! )
---
+++ Projects/School/PA6/main.cpp
@@ -0,0 +1,28 @@
+/** 
+ * Full Name: Dylan M. Ravel
+ * Student ID: 2445987
+ * Chapman Email: ravel@chapman.edu
+ * Course Number and Section: CPSC-350-03
+ * Assignment or Exercise Number: PA4: Genetic Palindromes
+*/
+
+#include <iostream>
+#include "FileProcessor.h"
+
+int main(int argc, char* argv[]) {
+    // Checks if no input file has been passed in
+    if(argc != 2) {
+        std::cerr << "[ERROR] - You ran the program incorrectly. Please try again with the following usage: '" << argv[0] << " INPUT_FILE.txt'" << std::endl;
+        return 1;
+    }
+
+    // Creates a file processor to use
+    FileProcessor* fileProcessor = new FileProcessor(argv[1]);
+    fileProcessor->processFile();
+
+    // Deletes the file processor
+    delete fileProcessor;
+
+    // Ends main method
+    return 0;
+}
