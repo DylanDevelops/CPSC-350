6433e85393b481a09a854342a7cd44e8ea5df848
Dylan Ravel <dylanm.ravel@gmail.com>
2024-12-11 17:15:54 -0800

    solved test problem

--- Scratchwork.cpp
+++ Scratchwork.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
-#include <vector>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
+string reverseString(string& input) {
+    stack<char> letters;
+    queue<int> digits;
+    string finalString = "";
+
+    for(int i = 0; i < input.size(); ++i)
+    {
+        if(isalpha(input[i])) {
+            letters.push(input[i]);
+        } else if(isdigit(input[i])) {
+            digits.push(input[i]);
+        }
+    }
+
+    while(!letters.empty()) {
+        finalString += letters.top();
+        letters.pop();
+    }
+
+    while(!digits.empty()) {
+        finalString += digits.front();
+        digits.pop();
+    }
+
+    return finalString;
+}
+
 int main() {
-    int num = 9;
+    string test = "a1b2c3";
 
-    string answer = num % 2 ? "O" : "E";
-
-    std::cout << answer << std::endl;
+    cout << reverseString(test) << endl;
 }
